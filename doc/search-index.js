161d3f819272e55844634c13754354b626201649
chris-pe <christophe.pedretti@gmail.com>
2014-05-02 17:09:06 +0200

    Documentation update

--- doc/search-index.js
+++ doc/search-index.js
@@ -1,3 +1,7 @@
 var searchIndex = {};
+<<<<<<< Updated upstream
 searchIndex['rustic'] = {"items":[[0,"","rustic",""],[0,"util","",""],[1,"Properties","rustic::util",""],[11,"props","","",0],[10,"new","","",0],[10,"setProperty","","",0],[10,"getProperty","","",0]],"paths":[[1,"Properties"]]};
+=======
+searchIndex['rustic'] = {"items":[[0,"","rustic","Complementary crate to standard crates providing basic and easy to use functionalities."],[0,"util","","Miscellaneous utility classes."],[1,"Properties","rustic::util",""],[11,"props","","",0],[10,"new","","",0],[10,"setProperty","","",0],[10,"getProperty","","",0]],"paths":[[1,"Properties"]]};
+>>>>>>> Stashed changes
 initSearch(searchIndex);
