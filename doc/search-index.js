4792978c05a76322ff7334766e2345e9b3368048
chris-pe <christophe.pedretti@gmail.com>
2014-05-02 17:49:29 +0200

    doc update

---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['rustic'] = {"items":[[0,"","rustic","Complementary crate to standard crates providing basic and easy to use functionalities."],[0,"util","","Miscellaneous utility classes."],[1,"Properties","rustic::util",""],[11,"props","","",0],[10,"new","","",0],[10,"setProperty","","",0],[10,"getProperty","","",0]],"paths":[[1,"Properties"]]};
+initSearch(searchIndex);
