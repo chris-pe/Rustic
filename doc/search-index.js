22570bdc61f9182024a48c1dc703c21b089b5d80
chris-pe <christophe.pedretti@gmail.com>
2014-05-02 16:04:21 +0200

    First API Documentation

---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['rustic'] = {"items":[[0,"","rustic",""],[0,"util","",""],[1,"Properties","rustic::util",""],[11,"props","","",0],[10,"new","","",0],[10,"setProperty","","",0],[10,"getProperty","","",0]],"paths":[[1,"Properties"]]};
+initSearch(searchIndex);
