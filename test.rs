53ad05b8902378a8642bd2735a8fa6262326afdf
chris-pe <christophe.pedretti@gmail.com>
2014-05-05 17:49:18 +0200

    properties load is ok except for multilines

---
+++ test.rs
@@ -0,0 +1,11 @@
+extern crate rustic;
+
+use std::io::{File, Open, Read};
+use rustic::util::Properties;
+
+fn main() {
+	let p = Path::new("test.properties");
+	let f = File::open_mode(&p, Open, Read);
+	let mut props = Properties::new();
+	println!("num:{}", props.load(f));
+}
--- util/mod.rs
+++ util/mod.rs
@@ -38,7 +38,7 @@ impl Properties {
 	</ul>
 	<u>Notes&nbsp;:</u>
 	<ul>
-	<li>a line terminator is either '\n' or '\r' or '\r\n'</li>
+	<li>a line terminator is either '\n' or '\r\n'</li>
 	<li>the following characters can be escaped&nbsp;: ASCII white-space '&#92;&nbsp;', line terminator '\r' or '\n', '&#92;#', '&#92;!', '\=', '&#92;:'</li>
 	<li>a key-element pair may be spread out across several adjacent natural lines by terminating the line with a backslash character '\'</li>
 	<pre class='rust fn'>targetCities=\
@@ -50,30 +50,33 @@ impl Properties {
 	*/
 	pub fn load<T : Reader>(&mut self,  reader : T)-> IoResult<uint> {
 		let mut t : IoResult<uint> = Ok(0); // Result<T, IoError>
-		//let mut key;
+		//let mut multi = ~"";
 		for line in BufferedReader::new(reader).lines() {
 			match line {
-				Ok(l) => {
-					let mut k = l.trim_left(); if k.char_len()==0 { continue; }
+				Ok(l) => {	
+					let mut buf = l.trim_left();	
+					if buf.starts_with("#") || buf.starts_with("!") { continue; } // Comment line
+					if buf.ends_with("\n") {
+						if buf.ends_with("\r\n") { buf=buf.slice_to(buf.len()-2); } // Line ends with '\r\n'
+							else { buf=buf.slice_to(buf.len()-1); } // Line ends with '\n'
+					}
+					if buf.len()==0 { continue; } //Empty line
+
 					let mut idx : uint = 0;
-					for c in k.chars() {if !c.is_whitespace() && c!='=' && c!=':' { idx=idx+1; continue; } }
-					let key = k.slice_to(idx); k=k.slice_from(idx); k = k.trim_left();
-					if k.char_len()==0 { self.props.insert(key.to_owned(), ~""); println!("'{}'=''", key); continue; }
-					if k.char_at(0) == '=' || k.char_at(0) == '=' {
-						k=k.slice_from(1);
-						k=k.trim_left();
-					}
-					if k.char_at_reverse(0)=='\n' ||  k.char_at_reverse(0)=='\r' {
-						if k.char_at_reverse(0)=='\r' { k=k.slice_to(k.char_len()-1); } // Line ends with '\r'
-						else if k.char_len()>1 && k.char_at_reverse(1)=='\r' { k=k.slice_to(k.char_len()-2); } // Line ends with '\r\n'
-							else { k=k.slice_to(k.char_len()-1); } // Line ends with '\n'
-					}
-					self.props.insert(key.to_owned(), k.to_owned());
-					println!("'{}'='{}'", key, k);
+					for c in buf.chars() { if !c.is_whitespace() && c!='=' && c!=':' { idx+=1; }
+											else { break; } }
+					let key = buf.slice_chars(0, idx);
+					buf = buf.slice_chars(idx, buf.char_len()); buf = buf.trim_left();
+					if buf.starts_with("=") || buf.starts_with(":") { buf=buf.slice_from(1); buf = buf.trim_left(); }					
+					self.props.insert(key.to_owned(), buf.to_owned()); println!("'{}'='{}'", key, buf);
+					//println!("DEBUG:{}'", l);
 				}
 				Err(e) => { t=Err(e); break; }
 			}
+			//println!("DEBUG:{}'", multi);
+			//multi=~"";
 		}
+		if t.is_ok() { t = Ok(self.props.len()); }
 		t
 	}
 }
