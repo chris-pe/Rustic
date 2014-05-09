cfac2063e1e785896b1fd122a768ae7b29488e5a
chris-pe <christophe.pedretti@gmail.com>
2014-05-09 18:44:26 +0200

    Doc migrated to rust-ci

--- doc/search-index.js
+++
@@ -1,3 +0,0 @@
-var searchIndex = {};
-searchIndex['rustic'] = {"items":[[0,"","rustic","Complementary crate to standard crates providing basic and easy to use functionalities."],[0,"util","","Miscellaneous utility classes."],[1,"Properties","rustic::util","Contains a list of properties. A property is a key-value pair."],[11,"props","","",0],[0,"sql","rustic","<b>WIP&nbsp;: </b>Provides the API for accessing and processing data stored in a data source (usually a relational database) using the Rust programming language."]],"paths":[[1,"Properties"]]};
-initSearch(searchIndex);
