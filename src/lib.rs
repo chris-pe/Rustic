8a061dafacf62bc684840426730fdfb408893727
chris-pe <christophe.pedretti@gmail.com>
2015-02-11 00:58:06 +0100

    Rust 1.0

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+//!Complementary crate to standard crates providing basic and easy to use functionalities.
+//!
+//!This project is at an early development stage, please do not use elements tagged as <i>WIP</i>. 
+#![crate_type = "lib"]
+#![crate_name = "rustic"]
+#![feature(std_misc,collections,core,io,libc)]
+
+extern crate collections;
+extern crate libc;
+
+///Miscellaneous utility classes.
+pub mod util {
+	pub use self::properties::Properties;
+	mod properties;
+}
+
+///Provides the API for accessing and processing data stored in a relational database.
+pub mod sql {
+	pub use self::connection::Connection;
+	pub use self::connection::Statement;
+	pub use self::connection::Cursor;
+	mod connection;
+	///Supported Databases
+	pub enum DbType {
+		SQLite3
+	}
+	impl Copy for DbType {}
+}
